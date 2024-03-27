4ede5fa71db632a9941652496707a6aa09cf35b3
Hoàng Nguyên <110187720+hoangnguyenhtng@users.noreply.github.com>
2024-03-27 22:36:53 +0700

    Add: frontend login, register, order, order-detail, homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,33 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OrderComponent } from './order/order.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    OrderComponent
+    OrderComponent,
+    DetailProductComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    //HomeComponent
+    //OrderComponent
+    //DetailProductComponent
+    //OrderConfirmComponent
+    //LoginComponent
+    RegisterComponent
+  ]
 })
 export class AppModule { }
